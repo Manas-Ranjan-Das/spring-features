6aad9b916386b39fbcc1941a883f8e7040a47128
Manas Ranjan Das <manasranjandas.student@gmail.com>
2025-04-25 09:23:00 +0530

    These commits are so that I can syanc between two laptops

--- test/Cat.java
+++ test/Cat.java
@@ -1,5 +1,18 @@
 package test;
 
 public class Cat {
-    
+    public String name;
+    public int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void meow() {
+        System.out.println(name + " says meow!");
+    }
+    public void purr() {
+        System.out.println(name + " is purring.");
+    }
 }
